359b5f5f6e42c65ab6729748d23bae43f645a686
Florea Rares <raresflorea05@gmail.com>
2025-11-07 12:24:55 +0200

    Start lab

--- app/src/main/java/com/example/phonedialer/PhoneDialerActivity.kt
+++ app/src/main/java/com/example/phonedialer/PhoneDialerActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PhoneDialerTheme {
-        Greeting("Android")
+        Greeting("Andre")
     }
 }
